9a7982ba35db627e1d7b3d7da91e652982e2d60d
JeremyBois <jeremybois@protonmail.com>
2019-02-12 13:22:59 +0100

    Add in game UI to construct Triangulation and Diagrams on the fly.

--- DelaunoiApplication/Assets/Scripts/Demo/GuibasStolfiPlane.cs
+++ DelaunoiApplication/Assets/Scripts/Demo/GuibasStolfiPlane.cs
@@ -2,18 +2,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuibasPlane : MonoBehaviour
+
+using Delaunoi;
+using Delaunoi.Generators;
+using Delaunoi.DataStructures;
+using Delaunoi.Tools;
+using Delaunoi.DataStructures.Extensions;
+using Delaunoi.Tools.Extensions;
+
+
+public class GuibasStolfiPlane : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
+    private List<Vec3> points;
+    private Mesh2D<int, int> meshUsed;
+    private PoissonDisk2D poissonGen;
 
+    // Settings
+    public int pointNumber = 10;
+    public int seed = 154;
+    public int[] bases = { 2, 3 };
+    public GeneratorType usedGenerator;
+    private int[] boundaries = { 300, 200 };
+    public float minimalDistance = 15.0f;
+    private int maxAttemp = 40;
+
+    // Drawing
+    [SerializeField]
+    private GameObject[] shapes = new GameObject[2];
+    [SerializeField]
+    private Material mat;
+    [SerializeField]
+    private Gradient gradient;
+    public float scale = 1.0f;
+    public float lineScale = 1.5f;
+	// Delaunay
+    public bool D_points = false;
+    public bool D_lines = false;
+    public bool D_faces = false;
+	// Voronoï
+    public FaceConfig celltype;
+    public bool V_points = false;
+    public bool V_lines = false;
+    public bool V_faces = false;
+
+    void Awake()
+    {
+        // // Place camera
+        // var cam = Camera.main;
+        // cam.transform.position = new Vector3(boundaries[0] / 2.0f, boundaries[1] / 2.0f, -0.8f * Mathf.Max(boundaries));
+    }
+
+	public void UpdateTriangulation()
+	{
+		foreach (Transform child in transform)
+		{
+			Destroy(child.gameObject);
+		}
+
+        BuildSample();
+        ConstructMesh();
+        DrawMesh();
+    }
+
+	void ConstructMesh()
+	{
+        meshUsed = new Mesh2D<int, int>(points.ToArray(), false);
+        meshUsed.Construct();
 	}
 
-	// Update is called once per frame
-	void Update ()
+	void DrawMesh()
 	{
+        // Draw Delaunay
+        var triangles = meshUsed.Triangles().ToList();
+        if (D_faces)
+        {
+            TriangleDrawer.DrawFace(triangles, transform, mat, gradient);
+        }
+        if (D_lines)
+        {
+            TriangleDrawer.DrawLine(triangles, transform, mat, Color.black, lineScale);
+        }
+        if (D_points)
+        {
+            TriangleDrawer.DrawPoints(triangles, transform, shapes[0], Color.red, 1.1f * scale);
+        }
 
+        // Get faces
+        List<Face<int, int>> faces = meshUsed.Faces(celltype, Mathf.Max(boundaries) * 5.0, true)
+                                             .ToList();
+
+        float nbCells = (float)faces.Count;
+        int indcolor2 = 0;
+        foreach (Face<int, int> face in faces)
+        {
+            var color = gradient.Evaluate(indcolor2 / nbCells);
+
+            if (V_faces)
+            {
+                face.DrawFace(transform, mat, color);
+            }
+            if (V_lines)
+            {
+                face.DrawLine(transform, mat, Color.white, lineScale, loop: true);
+            }
+            if (V_points)
+            {
+                face.DrawPoints(transform, shapes[1], mat, Color.blue, 0.8f * scale);
+            }
+            indcolor2++;
+        }
 	}
+
+    void BuildSample()
+    {
+        // Create random points
+        int n = seed;
+        points = new List<Vec3>();
+
+        switch (usedGenerator)
+        {
+            case GeneratorType.Halton:
+                for (int i = 0; i < pointNumber; i++)
+                {
+                    // Random sparse distribution
+                    Vec3 temp = HaltonSequence.Halton2D(n, bases[0], bases[1]);
+                    points.Add(new Vec3(temp.X * boundaries[0] + transform.position.x,
+                                         temp.Y * boundaries[1] + transform.position.y,
+                                         transform.position.z));
+                    n++;
+                }
+                break;
+            case GeneratorType.Poisson:
+                RandGen.Init(seed);
+                poissonGen = new PoissonDisk2D(minimalDistance, boundaries[0], boundaries[1],
+                                               maxAttemp);
+                poissonGen.BuildSample(pointNumber);
+                foreach (Vec3 point in poissonGen)
+                {
+                    points.Add(point);
+                }
+                break;
+        }
+    }
 }
--- DelaunoiApplication/Assets/Scripts/Demo/UIManager.cs
+++ DelaunoiApplication/Assets/Scripts/Demo/UIManager.cs
@@ -1,19 +1,260 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
+using Delaunoi;
+
 public class UIManager : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject ToolsCanvas;
+
+    // Generators
+    [SerializeField]
+    private GameObject GeneratorsWrapper;
+
+	[SerializeField]
+	private GuibasStolfiPlane DelaunoiHandler;
+
+	// Toggles
+    public Toggle VorPts;
+    public Toggle VorLines;
+    public Toggle VorFaces;
+    public Toggle DelPts;
+    public Toggle DelLines;
+    public Toggle DelFaces;
+
+	// Sliders
+    public Slider ShapesSlider;
+    public Slider LinesSlider;
+
+	// Dropdown
+	public Dropdown GeneratorTypeDrop;
+    public Dropdown FaceTypeDrop;
+
+
+	// Input fields
+	public InputField nbPointsInput;
+    public InputField seedInput;
+    public InputField HaltonField;
+    public InputField MinimalDistance;
+
 
 	// Use this for initialization
 	void Start ()
 	{
+        PrepareToggles();
+		PrepareSliders();
+        PrepareDropdown();
 
+        // Get values from handler
+        InitValues();
 	}
 
-	// Update is called once per frame
-	void Update ()
+    bool Coprime(int value1, int value2)
+    {
+        while (value1 != 0 && value2 != 0)
+        {
+            if (value1 > value2)
+                value1 %= value2;
+            else
+                value2 %= value1;
+        }
+        return Math.Max(value1, value2) == 1;
+    }
+
+	void InitValues()
 	{
+        // Sliders
+        LinesSlider.value = DelaunoiHandler.lineScale;
+        ShapesSlider.value = DelaunoiHandler.scale;
 
+        // Toggles
+        VorPts.isOn = DelaunoiHandler.V_points;
+        VorLines.isOn = DelaunoiHandler.V_lines;
+        VorFaces.isOn = DelaunoiHandler.V_faces;
+        DelPts.isOn = DelaunoiHandler.D_points;
+        DelLines.isOn = DelaunoiHandler.D_lines;
+        DelFaces.isOn = DelaunoiHandler.D_faces;
+
+        // Dropdown
+        GeneratorTypeDrop.value = (int)DelaunoiHandler.usedGenerator;
+        FaceTypeDrop.value = (int)DelaunoiHandler.celltype;
+    }
+
+
+	public void RegenerateDiagram()
+	{
+		if (nbPointsInput.text != String.Empty)
+		{
+            DelaunoiHandler.pointNumber = Convert.ToInt32(nbPointsInput.text);
+		}
+
+        if (seedInput.text != String.Empty)
+        {
+            DelaunoiHandler.seed = Convert.ToInt32(seedInput.text);
+        }
+
+        if (HaltonField.text != String.Empty)
+        {
+            int base1 = Convert.ToInt32(HaltonField.text);
+            int base2 = DelaunoiHandler.bases[0] + 1;
+
+            // Find closest coprime
+            int maxTest = 1;
+            while (!Coprime(base1, base2))
+            {
+                ++base2;
+                ++maxTest;
+            }
+
+            Debug.Log(base2);
+            if (maxTest < 10)
+            {
+                DelaunoiHandler.bases[0] = base1;
+                DelaunoiHandler.bases[1] = base2;
+            }
+
+        }
+
+        if (MinimalDistance.text != String.Empty)
+        {
+            DelaunoiHandler.minimalDistance = Convert.ToInt32(MinimalDistance.text);
+        }
+
+		// Ask handle to update data
+        DelaunoiHandler.UpdateTriangulation();
 	}
+
+	public void UpdateVoronoiPts(Toggle change)
+	{
+        DelaunoiHandler.V_points = change.isOn;
+	}
+
+    public void UpdateVoronoiLines(Toggle change)
+    {
+        DelaunoiHandler.V_lines = change.isOn;
+    }
+
+    public void UpdateVoronoiFaces(Toggle change)
+    {
+        DelaunoiHandler.V_faces = change.isOn;
+    }
+
+    public void UpdateDelaunayPts(Toggle change)
+    {
+        DelaunoiHandler.D_points = change.isOn;
+    }
+
+    public void UpdateDelaunayLines(Toggle change)
+    {
+        DelaunoiHandler.D_lines = change.isOn;
+    }
+
+    public void UpdateDelaunayFaces(Toggle change)
+    {
+        DelaunoiHandler.D_faces = change.isOn;
+    }
+
+    public void UpdateShapeSlider(Slider change)
+    {
+        DelaunoiHandler.scale = change.value;
+    }
+
+    public void UpdateLineSlider(Slider change)
+    {
+        DelaunoiHandler.lineScale = change.value;
+    }
+
+    public void UpdateFaceType(Dropdown change)
+    {
+        DelaunoiHandler.celltype = (FaceConfig)change.value;
+    }
+
+    public void UpdateGenerator(Dropdown change)
+    {
+        switch (change.value)
+        {
+            case 0:
+                GeneratorsWrapper.transform.GetChild(1).gameObject.SetActive(true);
+                GeneratorsWrapper.transform.GetChild(2).gameObject.SetActive(false);
+                DelaunoiHandler.usedGenerator = GeneratorType.Halton;
+                break;
+            case 1:
+                GeneratorsWrapper.transform.GetChild(1).gameObject.SetActive(false);
+                GeneratorsWrapper.transform.GetChild(2).gameObject.SetActive(true);
+                DelaunoiHandler.usedGenerator = GeneratorType.Poisson;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+	void PrepareDropdown()
+	{
+        FaceTypeDrop.onValueChanged.AddListener(delegate
+        {
+            UpdateFaceType(FaceTypeDrop);
+        });
+
+        GeneratorTypeDrop.onValueChanged.AddListener(delegate
+        {
+            UpdateGenerator(GeneratorTypeDrop);
+        });
+	}
+
+
+
+	void PrepareSliders()
+	{
+        ShapesSlider.onValueChanged.AddListener(delegate
+        {
+            UpdateShapeSlider(ShapesSlider);
+        });
+
+        LinesSlider.onValueChanged.AddListener(delegate
+        {
+            UpdateLineSlider(LinesSlider);
+        });
+	}
+
+
+	void PrepareToggles()
+	{
+        VorPts.onValueChanged.AddListener(delegate
+        {
+            UpdateVoronoiPts(VorPts);
+        });
+
+        VorLines.onValueChanged.AddListener(delegate
+        {
+            UpdateVoronoiLines(VorLines);
+        });
+
+        VorFaces.onValueChanged.AddListener(delegate
+        {
+            UpdateVoronoiFaces(VorFaces);
+        });
+
+        DelPts.onValueChanged.AddListener(delegate
+        {
+            UpdateDelaunayPts(DelPts);
+        });
+
+        DelLines.onValueChanged.AddListener(delegate
+        {
+            UpdateDelaunayLines(DelLines);
+        });
+
+        DelFaces.onValueChanged.AddListener(delegate
+        {
+            UpdateDelaunayFaces(DelFaces);
+        });
+	}
+
 }
